f4de61f784db890fc84709b075b8ec48dc4e1c22
tsurutan <tsurutan.android@gmail.com>
2024-08-29 09:09:17 +0900

    refactor: 💡 remove implementation

--- src/rps.test.ts
+++ src/rps.test.ts
@@ -1,34 +1,4 @@
-import {play, PlayArg} from '@/rps';
-import {describe, expect, test} from 'vitest';
+import {describe} from 'vitest';
 
-describe('rps', () => {
-    describe('player1 wins', (params) => {
-        test.each([
-            { player1Hand: 'rock', player2Hand: 'scissors'},
-            { player1Hand: 'scissors', player2Hand: 'paper'},
-            { player1Hand: 'paper', player2Hand: 'rock'},
-        ] as PlayArg[])('player1 should win when player 1 do $player1Hand and player 2 do $player2Hand', (params) => {
-            expect(play(params)).toBe("player1 wins")
-        })
-    })
-
-    describe('player2 wins', (params) => {
-        test.each([
-            { player1Hand: 'scissors', player2Hand: 'rock'},
-            { player1Hand: 'paper', player2Hand: 'scissors'},
-            { player1Hand: 'rock', player2Hand: 'paper'},
-        ] as PlayArg[])('player2 should win when player 1 do $player1Hand and player 2 do $player2Hand', (params) => {
-            expect(play(params)).toBe("player2 wins")
-        })
-    })
-
-    describe('draw', (params) => {
-        test.each([
-            { player1Hand: 'rock', player2Hand: 'rock'},
-            { player1Hand: 'scissors', player2Hand: 'scissors'},
-            { player1Hand: 'paper', player2Hand: 'paper'},
-        ] as PlayArg[])('it should be draw when player 1 do $player1Hand and player 2 do $player2Hand', (params) => {
-            expect(play(params)).toBe("draw")
-        })
-    })
+describe('Rock, Paper, Scissors', () => {
 })
--- src/rps.ts
+++ src/rps.ts
@@ -1,19 +1,2 @@
-
-type Kind = "rock" | "paper" | "scissors";
-type Result = "player1 wins" | "player2 wins" | "draw";
-export type PlayArg = {
-    player1Hand: Kind;
-    player2Hand: Kind;
-}
-
-export const play = ({ player1Hand, player2Hand }: PlayArg): Result => {
-    if(player1Hand === player2Hand) return 'draw';
-
-    if(
-        (player1Hand === 'paper' && player2Hand == 'rock') ||
-        (player1Hand === 'rock' && player2Hand == 'scissors') ||
-        (player1Hand === 'scissors' && player2Hand == 'paper')
-    ) return "player1 wins";
-
-    return 'player2 wins';
+export const play = () => {
 }
